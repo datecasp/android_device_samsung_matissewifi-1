b1157953413f71c59e35d55578f3e25497bbb2e2
sub77 <sub77@ymail.com>
2016-03-08 17:00:37 -0600

    Add IR HAL

--- device.mk
+++ device.mk
@@ -22,5 +22,13 @@ $(call inherit-product-if-exists, vendor/samsung/matissewifi/matissewifi-vendor.
 # Overlays
 DEVICE_PACKAGE_OVERLAYS += $(LOCAL_PATH)/overlay
 
+# Permissions
+PRODUCT_COPY_FILES += \
+    frameworks/native/data/etc/android.hardware.consumerir.xml:system/etc/permissions/android.hardware.consumerir.xml
+
+# IR
+PRODUCT_PACKAGES += \
+    consumerir.msm8226
+
 # common matisse
 $(call inherit-product, device/samsung/matisse-common/matisse.mk)
