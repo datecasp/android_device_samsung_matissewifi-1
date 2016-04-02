cd030e2cd450571ff7a0e7394a403f4bcb32a16f
sub77 <sub77@ymail.com>
2016-04-02 04:59:56 -0500

    remove IR HAL

--- device.mk
+++ device.mk
@@ -22,9 +22,5 @@ $(call inherit-product-if-exists, vendor/samsung/matissewifi/matissewifi-vendor.
 # Overlays
 DEVICE_PACKAGE_OVERLAYS += $(LOCAL_PATH)/overlay
 
-# Permissions
-PRODUCT_COPY_FILES += \
-    frameworks/native/data/etc/android.hardware.consumerir.xml:system/etc/permissions/android.hardware.consumerir.xml
-
 # common matisse
 $(call inherit-product, device/samsung/matisse-common/matisse.mk)
